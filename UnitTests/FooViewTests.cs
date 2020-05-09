8c680302295a33410942bda10a42b8a7146df227
Tery Lim <kumabotz@gmail.com>
2020-05-09 21:59:06 +1200

    Add unit tests

---
+++ UnitTests/FooViewTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using SampleApp.Views;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class FooViewTests : ViewUnitTestBase
+    {
+        [Test]
+        public void FooViewTest()
+        {
+            var view = new FooView();
+            Assert.NotNull(view);
+        }
+    }
+}
---
+++ UnitTests/ViewUnitTestBase.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using SampleApp;
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests
+{
+    public class ViewUnitTestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            MockForms.Init();
+            Application.Current = new App();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+    }
+}
